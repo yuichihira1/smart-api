d3c54459e7a4651f46b5ddccf249eafc8ea15872
hirayuichi <yhira5796@gmail.com>
2017-10-15 00:50:11 +0900

    usersテーブルを作成した。

--- db/migrate/20171014141335_create_users.rb
+++ db/migrate/20171014141335_create_users.rb
@@ -1,12 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :access_token
+      t.string  :name
+      t.string  :email
+      t.string  :access_token
       t.string  :password_digest
-      t.datetime :created_at
-      t.datetime :updated_at
+
 
       t.timestamps
     end
