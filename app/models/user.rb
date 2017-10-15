5d24cfd4f41316e57af05ef46f32a6a8c5ec461d
hirayuichi <yhira5796@gmail.com>
2017-10-15 22:49:32 +0900

    Userモデルにトークン生成機構を記述。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
 	has_secure_password validations: true
 
+	befpre_create do|user|
+		user.access_token = user.generate_auth_token
+	end
+
+	def generate_auth_token
+		loop do
+			token = SourceRandom.base64.tr('+/=', 'Qrt')
+			break token unless User.exixts?(access_token: token)
+		end
+	end
+
   validates :mail, presence: true, uniqueness: true
 end
