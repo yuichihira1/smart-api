275b000cbaa4cb903eda10dbb38dbc778f0d4de3
hirayuichi <yhira5796@gmail.com>
2017-10-16 00:03:50 +0900

    セッション周りのルーティングを設定。

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
    scope module: :api, defaults: { format: :json } do
    	namespace :v1 do
    		resources :users, only: [:index, :create, :show]
+   		resource :login, only: [:create], controller: :sessions
+   		resource :session, only: [:show], controller: :sessions
 		end
 	end
 end
