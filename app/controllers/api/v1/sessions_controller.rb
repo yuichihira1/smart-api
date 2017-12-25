995174f4c91474e8c56159f4ae76d7746e3fbdcf
hirayuichi <yhira5796@gmail.com>
2017-12-25 10:43:59 +0900

    セッション部分のリファクタリング

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,17 +1,15 @@
-module Api::V1
+module API::V1
   class API::V1::SessionsController < ApplicationController
   	skip_before_action :authenticate
 
-  	def create
-        @user = User.find_for_database_authentication(email: params[:email])
-        return invalid_email unless @user
 
-        if @user.valid_password?(params[:password])
-          sign_in :user, @user
-          render json: @user, serializer: UserSerializer, root: nil
+  	def create
+        @user = User.find_by(email: params[:email])
+        if @user = nil
+          render json: { error: t('user_session_error') }, status: :unprocessable_entity
         else
-          invalid_password
-        end
+          render json: @user, serializer: UserSerializer, root: nil
+         end
       end
-  end
+    end
 end
